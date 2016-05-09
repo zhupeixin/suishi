ac036a03078a252daa502d660f705a53613fca6b
= <2430823381@qq.com>
2016-05-09 19:36:08 +0800

    图片适应，添加三级空白页面

--- www/js/app.js
+++ www/js/app.js
@@ -26,11 +26,16 @@ angular.module('starter', ['ionic','starter.controllers','starter.services'])
   .config(function($stateProvider, $urlRouterProvider){
     //一个页面为一个状态，在下面配置其路径，控制器等，添加了首页和第一个跳转页的示例
     $stateProvider
-      .state('start', {
-        url: '/start',
-        templateUrl: 'templates/start.html',
-        controller: "startCtrl"
-      })
+          .state('start', {
+            url: '/start',
+            templateUrl: 'templates/start.html',
+            controller: "startCtrl"
+          })
+        .state('first',{
+            url: '/first',
+            templateUrl: 'templates/first.html',
+            controller: "firstCtrl"
+        })
         .state('second_1', {
             url: '/second_1',
             templateUrl: 'templates/second_1.html',
@@ -51,17 +56,131 @@ angular.module('starter', ['ionic','starter.controllers','starter.services'])
             templateUrl: 'templates/second_4.html',
             controller: "second_4Ctrl"
         })
+        .state('three_1_1', {
+            url: '/three_1_1',
+            templateUrl: 'templates/three_1_1.html',
+            controller: "three_1_1Ctrl"
+        })
+        .state('three_1_2', {
+            url: '/three_1_2',
+            templateUrl: 'templates/three_1_2.html',
+            controller: "three_1_2Ctrl"
+        })
+        .state('three_1_3', {
+            url: '/three_1_3',
+            templateUrl: 'templates/three_1_3.html',
+            controller: "three_1_3Ctrl"
+        })
+        .state('three_1_4', {
+            url: '/three_1_4',
+            templateUrl: 'templates/three_1_4.html',
+            controller: "three_1_4Ctrl"
+        })
+        .state('three_1_5', {
+            url: '/three_1_5',
+            templateUrl: 'templates/three_1_5.html',
+            controller: "three_1_5Ctrl"
+        })
+        .state('three_1_6', {
+            url: '/three_1_6',
+            templateUrl: 'templates/three_1_6.html',
+            controller: "three_1_6Ctrl"
+        })
+        .state('three_2_1', {
+            url: '/three_2_1',
+            templateUrl: 'templates/three_2_1.html',
+            controller: "three_2_1Ctrl"
+        })
+        .state('three_2_2', {
+            url: '/three_2_2',
+            templateUrl: 'templates/three_2_2.html',
+            controller: "three_2_2Ctrl"
+        })
+        .state('three_2_3', {
+            url: '/three_2_3',
+            templateUrl: 'templates/three_2_3.html',
+            controller: "three_2_3Ctrl"
+        })
+        .state('three_2_4', {
+            url: '/three_2_4',
+            templateUrl: 'templates/three_2_4.html',
+            controller: "three_2_4Ctrl"
+        })
+        .state('three_2_5', {
+            url: '/three_2_5',
+            templateUrl: 'templates/three_2_5.html',
+            controller: "three_2_5Ctrl"
+        })
+        .state('three_2_6', {
+            url: '/three_2_6',
+            templateUrl: 'templates/three_2_6.html',
+            controller: "three_2_6Ctrl"
+        })
+        .state('three_3_1', {
+            url: '/three_3_1',
+            templateUrl: 'templates/three_3_1.html',
+            controller: "three_3_1Ctrl"
+        })
+        .state('three_3_2', {
+            url: '/three_3_2',
+            templateUrl: 'templates/three_3_2.html',
+            controller: "three_3_2Ctrl"
+        })
+        .state('three_3_3', {
+            url: '/three_3_3',
+            templateUrl: 'templates/three_3_3.html',
+            controller: "three_3_3Ctrl"
+        })
+        .state('three_3_4', {
+            url: '/three_3_4',
+            templateUrl: 'templates/three_3_4.html',
+            controller: "three_3_4Ctrl"
+        })
+        .state('three_3_5', {
+            url: '/three_3_5',
+            templateUrl: 'templates/three_3_5.html',
+            controller: "three_3_5Ctrl"
+        })
+        .state('three_3_6', {
+            url: '/three_3_6',
+            templateUrl: 'templates/three_3_6.html',
+            controller: "three_3_6Ctrl"
+        })
+        .state('three_4_1', {
+            url: '/three_4_1',
+            templateUrl: 'templates/three_4_1.html',
+            controller: "three_4_1Ctrl"
+        })
+        .state('three_4_2', {
+            url: '/three_4_2',
+            templateUrl: 'templates/three_4_2.html',
+            controller: "three_4_2Ctrl"
+        })
+        .state('three_4_3', {
+            url: '/three_4_3',
+            templateUrl: 'templates/three_4_3.html',
+            controller: "three_4_3Ctrl"
+        })
+        .state('three_4_4', {
+            url: '/three_4_4',
+            templateUrl: 'templates/three_4_4.html',
+            controller: "three_4_4Ctrl"
+        })
+        .state('three_4_5', {
+            url: '/three_4_5',
+            templateUrl: 'templates/three_4_5.html',
+            controller: "three_4_5Ctrl"
+        })
+        .state('three_4_6', {
+            url: '/three_4_6',
+            templateUrl: 'templates/three_4_6.html',
+            controller: "three_4_6Ctrl"
+        })
         .state('banquan',{
             url: '/banquan',
             templateUrl: 'templates/banquan.html',
             controller: "banquanCtrl"
-        })
-      .state('first',{
-        url: '/first',
-        templateUrl: 'templates/first.html',
-        controller: "firstCtrl"
-      });
-
+        });
 
     //当没有状态匹配到时默认显示的状态
     $urlRouterProvider.otherwise('/start');
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers', [])
 //在这下面添加页面对应的控制器，每个控制器管理对应页面的功能，跳转等等
   .controller('startCtrl', function($scope, $state, showMsgService) {
-    showMsgService.showMsg("首页控制器是startCtrl");
+    //showMsgService.showMsg("首页控制器是startCtrl");
     $scope.goFirst = function() {
       $state.go('first');
     }
@@ -29,21 +29,121 @@ angular.module('starter.controllers', [])
     .controller('second_1Ctrl',function($scope ,$state) {
         $scope.goBanQuan = function() {
             $state.go('banquan');
-        }
+        };
+        $scope.goThree_1_1 = function() {
+            $state.go('three_1_1');
+        };
+        $scope.goThree_1_2 = function() {
+            $state.go('three_1_2');
+        };
+        $scope.goThree_1_3 = function() {
+            $state.go('three_1_3');
+        };
+        $scope.goThree_1_4 = function() {
+            $state.go('three_1_4');
+        };
+        $scope.goThree_1_5 = function() {
+            $state.go('three_1_5');
+        };
+        $scope.goThree_1_6 = function() {
+            $state.go('three_1_6');
+        };
     })
     .controller('second_2Ctrl',function($scope ,$state) {
         $scope.goBanQuan = function() {
             $state.go('banquan');
-        }
+        };
+        $scope.goThree_2_1 = function() {
+            $state.go('three_2_1');
+        };
+        $scope.goThree_2_2 = function() {
+            $state.go('three_2_2');
+        };
+        $scope.goThree_2_3 = function() {
+            $state.go('three_2_3');
+        };
+        $scope.goThree_2_4 = function() {
+            $state.go('three_2_4');
+        };
+        $scope.goThree_2_5 = function() {
+            $state.go('three_2_5');
+        };
+        $scope.goThree_2_6 = function() {
+            $state.go('three_2_6');
+        };
     })
     .controller('second_3Ctrl',function($scope ,$state) {
         $scope.goBanQuan = function() {
             $state.go('banquan');
-        }
+        };
+        $scope.goThree_3_1 = function() {
+            $state.go('three_3_1');
+        };
+        $scope.goThree_3_2 = function() {
+            $state.go('three_3_2');
+        };
+        $scope.goThree_3_3 = function() {
+            $state.go('three_3_3');
+        };
+        $scope.goThree_3_4 = function() {
+            $state.go('three_3_4');
+        };
+        $scope.goThree_3_5 = function() {
+            $state.go('three_3_5');
+        };
+        $scope.goThree_3_6 = function() {
+            $state.go('three_3_6');
+        };
     })
     .controller('second_4Ctrl',function($scope ,$state) {
         $scope.goBanQuan = function() {
             $state.go('banquan');
-        }
+        };
+        $scope.goThree_4_1 = function() {
+            $state.go('three_4_1');
+        };
+        $scope.goThree_4_2 = function() {
+            $state.go('three_4_2');
+        };
+        $scope.goThree_4_3 = function() {
+            $state.go('three_4_3');
+        };
+        $scope.goThree_4_4 = function() {
+            $state.go('three_4_4');
+        };
+        $scope.goThree_4_5 = function() {
+            $state.go('three_4_5');
+        };
+        $scope.goThree_4_6 = function() {
+            $state.go('three_4_6');
+        };
+    })
+    .controller('three_1_1Ctrl',function($scope ,$state) {})
+    .controller('three_1_2Ctrl',function($scope ,$state) {})
+    .controller('three_1_3Ctrl',function($scope ,$state) {})
+    .controller('three_1_4Ctrl',function($scope ,$state) {})
+    .controller('three_1_5Ctrl',function($scope ,$state) {})
+    .controller('three_1_6Ctrl',function($scope ,$state) {})
+    .controller('three_2_1Ctrl',function($scope ,$state) {})
+    .controller('three_2_2Ctrl',function($scope ,$state) {})
+    .controller('three_2_3Ctrl',function($scope ,$state) {})
+    .controller('three_2_4Ctrl',function($scope ,$state) {})
+    .controller('three_2_5Ctrl',function($scope ,$state) {})
+    .controller('three_2_6Ctrl',function($scope ,$state) {})
+    .controller('three_3_1Ctrl',function($scope ,$state) {})
+    .controller('three_3_2Ctrl',function($scope ,$state) {})
+    .controller('three_3_3Ctrl',function($scope ,$state) {})
+    .controller('three_3_4Ctrl',function($scope ,$state) {})
+    .controller('three_3_5Ctrl',function($scope ,$state) {})
+    .controller('three_3_6Ctrl',function($scope ,$state) {})
+    .controller('three_4_1Ctrl',function($scope ,$state) {})
+    .controller('three_4_2Ctrl',function($scope ,$state) {})
+    .controller('three_4_3Ctrl',function($scope ,$state) {})
+    .controller('three_4_4Ctrl',function($scope ,$state) {})
+    .controller('three_4_5Ctrl',function($scope ,$state) {})
+    .controller('three_4_6Ctrl',function($scope ,$state) {})
+    .controller('banquanCtrl',function($scope ,$state) {
+
     });
+
 
